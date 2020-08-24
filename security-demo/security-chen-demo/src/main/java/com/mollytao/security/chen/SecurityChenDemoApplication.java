b2bb7b2be153ff50f0b248672c208f6585a028b3
Stephen Jiang <398665255@qq.com>
2020-08-25 06:59:56 +0800

    新增：登录demo

---
+++ security-demo/security-chen-demo/src/main/java/com/mollytao/security/chen/SecurityChenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mollytao.security.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityChenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityChenDemoApplication.class, args);
+    }
+
+}
