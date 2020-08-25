ab2870b3233833446c4180bbd7835692d211541c
Stephen Jiang <398665255@qq.com>
2020-08-26 06:58:01 +0800

    新增：登录与注销demo

---
+++ security-demo/security-auto-login-logout/src/main/java/com/mollytao/cloud/security/SecurityAutoLoginLogoutApplication.java
@@ -0,0 +1,13 @@
+package com.mollytao.cloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAutoLoginLogoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAutoLoginLogoutApplication.class, args);
+	}
+
+}
